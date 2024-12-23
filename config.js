6bb6636b3d904e3dad7c54a09f9f55eb5ade402d
Charuka1 <129839125+Charuka1@users.noreply.github.com>
2024-12-23 22:14:21 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+
+module.exports = {
+SESSION_ID : process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID ,
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? "" : process.env.GITHUB_AUTH_TOKEN,
+GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? "" : process.env.GITHUB_USERNAME,
+BOT_NUMBER: process.env.BOT_NUMBER === undefined ? "" : process.env.BOT_NUMBER,
+};
